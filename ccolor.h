1ba7dc0bc4f385814f4a3fdc967f04be6e5ca2f9
Dev Spain <9984398@gmail.com>
2022-08-11 12:55:23 +0300

    NameSpace Color : Completed

--- ccolor.h
+++ ccolor.h
@@ -4,16 +4,25 @@
 #include <iostream>
 
 
+
 namespace Color {
     enum Code {
         FG_RED = 31,
         FG_GREEN = 32,
+        FG_PINK = 33,
+        FG_PURPLE = 35,
         FG_BLUE = 34,
         FG_DEFAULT = 39,
         BG_RED = 41,
         BG_GREEN = 42,
+        BG_PIK = 43,
         BG_BLUE = 44,
-        BG_DEFAULT = 49
+        BG_PURPLE = 45,
+        BG_DEFAULT = 49,
+        SLOW_BLINK = 5,
+        FAST_BLINK = 6,
+        RESET = 0
+
     };
 
     class Modifier {
@@ -27,4 +36,4 @@ namespace Color {
     };
 }
 
-#endif CONSOLE_COLOR_H
+#endif //CONSOLE_COLOR_H
